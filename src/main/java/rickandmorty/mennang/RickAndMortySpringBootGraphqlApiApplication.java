ba97a90f86b5ad932a612caaaaad5db595528396
Mennan Güder <mennanguder@gmail.com>
2022-09-27 11:26:36 +0300

    Spring Initializr project creation for GraphQL API development for Rick and Morty characters dependencies: h2: embedded in-memory database faker: fake data creation lombok: reduce boilerplate code

---
+++ src/main/java/rickandmorty/mennang/RickAndMortySpringBootGraphqlApiApplication.java
@@ -0,0 +1,12 @@
+package rickandmorty.mennang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortySpringBootGraphqlApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortySpringBootGraphqlApiApplication.class, args);
+    }
+}
