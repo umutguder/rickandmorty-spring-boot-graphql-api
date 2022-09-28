3f43ab15bc65617d206632f8678459e424ec1e6c
Mennan Güder <mennanguder@gmail.com>
2022-09-28 17:09:48 +0300

    The baseline RickAndMorty api documentation behaves as the first page for all page requests < 2 that convension is coded

--- src/main/java/rickandmorty/mennang/model/responsepage/PageRequestAdapter.java
+++ src/main/java/rickandmorty/mennang/model/responsepage/PageRequestAdapter.java
@@ -3,11 +3,17 @@ package rickandmorty.mennang.model.responsepage;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
+/*
+PageRequestAdapter: Page request adapter for the documentation
+The baseline RickAndMorty api documentation behaves as the first page for all page requests < 2
+ */
 public class PageRequestAdapter extends PageRequest {
 
     protected PageRequestAdapter(int page, int size, Sort sort) {
-
-        // All page < 2 values are considered as page = 0
         super(page < 1 ? 0 : page - 1, size, sort);
+    }
+
+    public static PageRequest of(int page, int size) {
+        return PageRequest.of(page < 1 ? 0 : page - 1, size);
     }
 }
